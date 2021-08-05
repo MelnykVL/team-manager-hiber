fb68d959f0f66896ee2b682d6760cebc20cb9ea5
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-08-05 21:02:06 +0300

    Implement DeveloperRepositoryImpl.java

--- src/main/java/com/melnyk/teammanager/repository/implementation/DeveloperRepositoryImpl.java
+++ src/main/java/com/melnyk/teammanager/repository/implementation/DeveloperRepositoryImpl.java
@@ -1,33 +1,59 @@
 package com.melnyk.teammanager.repository.implementation;
 
 import com.melnyk.teammanager.model.Developer;
+import com.melnyk.teammanager.model.Skill;
 import com.melnyk.teammanager.repository.DeveloperRepository;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DeveloperRepositoryImpl implements DeveloperRepository {
     @Override
     public Developer getById(Integer integer) {
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        Developer dev = session.get(Developer.class, integer);
+        tx.commit();
+        session.close();
+        return dev;
     }
 
     @Override
     public Developer add(Developer developer) {
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        session.save(developer);
+        return developer;
     }
 
     @Override
     public Developer update(Developer developer) {
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        session.update(developer);
+        return developer;
     }
 
     @Override
     public Developer removeById(Integer integer) {
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        Developer dev = session.load(Developer.class, integer);
+        session.delete(dev);
+        tx.commit();
+        session.close();
+        return dev;
     }
 
     @Override
     public Set<Developer> getAll() {
-        return null;
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+        Set<Developer> developers = new HashSet<>(session.createQuery("FROM Developer").getResultList());
+        tx.commit();
+        session.close();
+        return developers;
     }
 }
